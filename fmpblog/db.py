c7b157bf69c15ac1fd5cd7a4b69f5a2ef271143d
kurur <kururu.fm@gmail.com>
2021-11-19 18:22:55 +0100

    Initial commit

---
+++ fmpblog/db.py
@@ -0,0 +1,9 @@
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sgecruddjango',  # nombre de la BBDD
+        'USER': 'root',  # tu usuario
+        'PASSWORD': 'root',  # tu password
+        'PORT': '3307'
+    }
+}
